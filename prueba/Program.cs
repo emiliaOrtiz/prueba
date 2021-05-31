fe1b0a453d9b73cdf8dd98e293c523c9d56b619f
emilia <rikukim@gmail.com>
2021-05-31 18:19:08 -0300

    initial commit

---
+++ prueba/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: rikuk
+ * Date: 28/5/2021
+ * Time: 21:48
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace prueba
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int indicador=0;
+			
+			do{
+			
+			Console.WriteLine("Ingrese nombre");
+			string nombre=Console.ReadLine();
+			Console.WriteLine("Hello "+nombre);
+			Console.ReadKey(true);
+			
+			Console.WriteLine("continuar : 1 o 0");
+			indicador=int.Parse(Console.ReadLine());
+			}
+			
+			while(indicador.Equals(1));
+			
+			
+		}
+	}
+}
